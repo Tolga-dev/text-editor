4eca96d6a41831e9c1625051d69904ae9714872f
Your Name <you@example.com>
2022-09-15 00:11:57 +0300

    changing read me

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # text-editor
 hobi project, basic text editor 
+basic implamatation of sfml
+
+#general brainstorm
+
+pseudocode:
+
+    main loop (while):
+        keypress events;
+        in response to keypress: (ok look like python :D)
+            update event again again
+            if quit == quit was pressed;
+                exit();
+        refresh;
+    
+
--- main.cpp
+++ main.cpp
@@ -3,23 +3,23 @@
 
 int main()
 {
-    sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
-    sf::CircleShape shape(100.f);
-    shape.setFillColor(sf::Color::Green);
+    //opening a window
+    sf::Window window;
+    window.create(sf::VideoMode(800, 600), "WIN");
 
-    while (window.isOpen())
+    while(window.isOpen())
     {
         sf::Event event;
-        while (window.pollEvent(event))
+
+        while(window.pollEvent(event))
         {
             if (event.type == sf::Event::Closed)
                 window.close();
         }
 
-        window.clear();
-        window.draw(shape);
-        window.display();
     }
+
+
 
     return 0;
 }
